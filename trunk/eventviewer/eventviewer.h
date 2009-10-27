a58f8057454acd42c8866c921de40edfaf9c21c7
tbranch88@gmail.com <tbranch88@gmail.com@581bd1f0-b7fa-11de-a433-7fce7b4d64d6>
2009-10-27 15:57:19 +0000

    Getter methods for everything

--- trunk/eventviewer/eventviewer.h
+++ trunk/eventviewer/eventviewer.h
@@ -120,9 +120,63 @@ protected:
 
 
 public:
-    eventviewer(TTree *treein);
+	Float_t * GetnumElectrons() { return &numElectrons; }
+	Float_t * GetnumJets() { return &numJets; }
+	Float_t * GetnumMuons() { return &numMuons ; }
+	Float_t * GetnumTaus() { return &numTaus; }
+	Float_t * GettriggerHLTIsoEle15() { return &triggerHLTIsoEle15; }
+	Float_t * GettriggerHLTIsoMu11() { return &triggerHLTIsoMu11; }
+	Float_t * GettriggerHLTMu15() { return &triggerHLTMu15; }
 	
+	std::vector<double> * GetelectronCharge() { return electronCharge;}
+	std::vector<double> * GetelectronCombinedIso() { return electronCombinedIso;}
+	std::vector<double> * GetelectronESuperOverP() { return electronESuperOverP;}
+	std::vector<double> * GetelectronEcalIso() { return electronEcalIso;}
+	std::vector<double> * GetelectronFBrem() { return electronFBrem;}
+	std::vector<double> * GetelectronHOverE() { return electronHOverE;}
+	std::vector<double> * GetelectronHcalIso() { return electronHcalIso;}
+	std::vector<double> * GetelectronSigmaIEtaIEta() { return electronSigmaIEtaIEta;}
+	std::vector<double> * GetelectronSuperEta() { return electronSuperEta;}
+	std::vector<double> * GetelectronTrackIso() { return electronTrackIso;}
+	std::vector<double> * GetjetBTagCombinedSecondary() { return jetBTagCombinedSecondary;}
+	std::vector<double> * GetjetBTagSimpleSecondary() { return jetBTagSimpleSecondary;}
+	std::vector<double> * GetjetBTagTrackCountHighEff() { return jetBTagTrackCountHighEff;}
+	std::vector<double> * GetjetBTagTrackCountHighPurity() { return jetBTagTrackCountHighPurity;}
+	std::vector<double> * GetmuonCaloCompatibility() { return muonCaloCompatibility;}
+	std::vector<double> * GetmuonCharge() { return muonCharge;}
+	std::vector<double> * GetmuonEcalIso() { return muonEcalIso;}
+	std::vector<double> * GetmuonHcalIso() { return muonHcalIso;}
+	std::vector<double> * GetmuonSegCompatibility() { return muonSegCompatibility;}
+	std::vector<double> * GetmuonTrackIso() { return muonTrackIso;}
+	std::vector<double> * GettauCharge() { return tauCharge;}
+	std::vector<double> * GettauECALIso() { return tauECALIso;}
+	std::vector<double> * GettauElectron() { return tauElectron;}
+	std::vector<double> * GettauLeadTrk() { return tauLeadTrk;}
+	std::vector<double> * GettauMuon() { return tauMuon;}
+	std::vector<double> * GettauTrackIso() { return tauTrackIso;}
+	std::vector<double> * GettauTracks() { return tauTracks;}
+	
+	TClonesArray * Getlv_electron() { return lv_electron;}
+	TClonesArray * Getlv_genbjet() { return lv_genbjet;}
+	TClonesArray * Getlv_genelectron() { return lv_genelectron;}
+	TClonesArray * Getlv_genmet() { return lv_genmet;}
+	TClonesArray * Getlv_genmuon() { return lv_genmuon;}
+	TClonesArray * Getlv_gentau() { return lv_gentau;}
+	TClonesArray * Getlv_gentaujet() { return lv_gentaujet;}
+	TClonesArray * Getlv_jet() { return lv_jet;}
+	TClonesArray * Getlv_met() { return lv_met;}
+	TClonesArray * Getlv_muon() { return lv_muon;}
+	TClonesArray * Getlv_pfmet() { return lv_pfmet;}
+	TClonesArray * Getlv_tau() { return lv_tau;}
+	TClonesArray * Getlv_tcmet() { return lv_tcmet;}
+	
+	
+	
+	eventviewer(TTree *treein);
+		
 	void GetEntry(ULong64_t entry){tree->GetTree()->GetEntry(entry, 0);}
+	
+	void Show();
 	
 
 };
