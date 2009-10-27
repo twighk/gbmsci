173595f6d525b494658eea855d11571ec4a41d3a
tbranch88@gmail.com <tbranch88@gmail.com@581bd1f0-b7fa-11de-a433-7fce7b4d64d6>
2009-10-27 14:03:14 +0000

    Added makefile to eventviewer

--- eventviewer/eventviewer.h
+++ eventviewer/eventviewer.h
@@ -1,19 +1,78 @@
 #ifndef __eventviewer__
 #define __eventviewer__
 
+#include <vector>
+
 #include <TTree.h>
 
 class eventviewer{
 private:
+//Temparary storage of leafs : Start
+	Float_t				numElectrons,
+						numJets,
+						numMuons, 
+						numTaus,
+						triggerHLTIsoEle15,
+						triggerHLTIsoMu11,
+						triggerHLTMu15;
+	std::vector<double>	*electronCharge,
+						*electronCombinedIso,
+						*electronESuperOverP,
+						*electronEcalIso,
+						*electronFBrem,
+						*electronHOverE,
+						*electronHcalIso,
+						*electronSigmaIEtaIEta,
+						*electronSuperEta,
+						*electronTrackIso,
+						*jetBTagCombinedSecondary,
+						*jetBTagSimpleSecondary,
+						*jetBTagTrackCountHighEff,
+						*jetBTagTrackCountHighPurity,
+						*muonCaloCompatibility,
+						*muonCharge,
+						*muonEcalIso,
+						*muonHcalIso,
+						*muonSegCompatibility,
+						*muonTrackIso,
+						*tauCharge,
+						*tauECALIso,
+						*tauElectron,
+						*tauLeadTrk,
+						*tauMuon,
+						*tauTrackIso,
+						*tauTracks;
+	TClonesArray		*lv_electron,
+						*lv_genbjet,
+						*lv_genelectron,
+						*lv_genmet,
+						*lv_genmuon,
+						*lv_gentau,
+						*lv_gentaujet,
+						*lv_jet,
+						*lv_met,
+						*lv_muon,
+						*lv_pfmet,
+						*lv_tau,
+						*lv_tcmet;
+//Temparary storage of leafs : End
+	
+	
     eventviewer();
     TTree *tree;
+	
+protected:
+	void bind() {
+		
+	}
 
 
 public:
     eventviewer(TTree *treein){
 		tree = treein;
 	}
-	
+
+#warning "Testfunction"
 	void test(){
 		tree->Show(0);
 	}
