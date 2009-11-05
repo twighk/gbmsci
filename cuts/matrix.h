d3df68887f2092c2e55ec50541dad81aae6aa3cc
tbranch88@gmail.com <tbranch88@gmail.com@581bd1f0-b7fa-11de-a433-7fce7b4d64d6>
2009-11-05 13:48:12 +0000

    more matrix stuff, andcheck whether one element is 1

--- cuts/matrix.h
+++ cuts/matrix.h
@@ -17,10 +17,10 @@ private:
 	
 	void matrixinit(Int_t _x, Int_t _y){
 		x = _x, y = _y; // set size internal
-		mat.resize(y); // make correct numer of rows(y)
-		for(Int_t i = 0; i != y; i++) {
-			mat[i].resize(x); // make correct numer of columns
-			for (Int_t j = 0; j != x; j++) {
+		mat.resize(x); // make correct numer of rows(y)
+		for(Int_t i = 0; i != x; i++) {
+			mat[i].resize(y); // make correct numer of columns
+			for (Int_t j = 0; j != y; j++) {
 				mat[i][j] = -1;
 			}
 		}
@@ -28,20 +28,54 @@ private:
 	
 public:
 	matrix(Int_t _x, Int_t _y = 1){ // matrix or vector // for scalar use vector size (1)
-		if (_x < 1) {std::cerr << "matrix not alowed x<0 size, so x set = 1" << std::endl; _x=1;}
-		if (_y < 1) {std::cerr << "matrix not alowed y<0 size, so y set = 1" << std::endl; _y=1;}
+		if (_x < 1) {std::cerr << "matrix not alowed x < 0 size, so x set = 1" << std::endl; _x=1;}
+		if (_y < 1) {std::cerr << "matrix not alowed y < 0 size, so y set = 1" << std::endl; _y=1;}
 		matrixinit(_x, _y);
 	} 
 	
+	Int_t Getx(){return x;}
+	Int_t Gety(){return y;}
+	
+	matrix operator&& (matrix &matin){
+		if ((matin.Getx()) != x || (matin.Gety() != y)) {
+			std::cerr << "matrixes not of equal dimentions" << std::endl;
+		}
+		
+		matrix matout(x,y);
+		
+		for (int i = 0; i != x; i++) {
+			for (int j = 0; j != y; j++) {
+				if (mat[i][j] == matin(i,j)) {
+					matout(i,j) = mat[i][j];
+				} else {
+					matout(i,j) = 0;
+				}
+
+			}
+		}
+		return matout;
+	}
+	
+	bool onecheck(){
+		for (int i = 0; i != x; i++) {
+			for (int j = 0; j != y; j++) {
+				if (mat[i][j] == 1) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
 	//direct access to elements // do the inversion of _x,_y here for users
 	T& operator()(){return mat[0][0];} // scalar
-	T& operator()(Int_t _x){return mat[0][_x];} // vector
-	T& operator()(Int_t _x, Int_t _y){return mat[_y][_x];} // matrix
+	T& operator()(Int_t _x){return mat[_x][0];} // vector
+	T& operator()(Int_t _x, Int_t _y){return mat[_x][_y];} // matrix
 	
 	//for easy outputting 
 	friend std::ostream& operator<< (std::ostream& out, matrix& mat){
-		for (Int_t i = 0; i != mat.y; i++) {
-			for (Int_t j = 0; j != mat.x; j++) {
+		for (Int_t i = 0; i != mat.x; i++) {
+			for (Int_t j = 0; j != mat.y; j++) {
 				out << mat.mat[i][j] << ' ';
 			}
 			out << std::endl;
