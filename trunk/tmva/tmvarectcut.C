666339ede566ee9e1d6ac1d3bfe470c5491ce0fe
tbranch88@gmail.com <tbranch88@gmail.com@581bd1f0-b7fa-11de-a433-7fce7b4d64d6>
2009-11-16 14:11:32 +0000

    added magic includes

--- trunk/tmva/tmvarectcut.C
+++ trunk/tmva/tmvarectcut.C
@@ -1,9 +1,28 @@
-#include "TMVAGui.C"
-#include "TMVA/Tools.h"
-#include "TMVA/Factory.h"
+#include <cstdlib>
+#include <iostream> 
+#include <map>
+#include <string>
 
-void tmvarectcut(){
+// idenify at a later point in time
+#include "TChain.h" // magic includes
+#include "TFile.h" // magic includes
+#include "TTree.h" // magic includes
+#include "TString.h" // magic includes
+#include "TObjString.h" // magic includes
+#include "TSystem.h" // magic includes
+#include "TROOT.h" // magic includes
+#include "TPluginManager.h" // magic includes
+
+#include "TMVAGui.C"
+
+#if not defined(__CINT__) || defined(__MAKECINT__)
+// needs to be included when makecint runs (ACLIC)
+#include "TMVA/Factory.h"
+#include "TMVA/Tools.h"
+#endif
+
+int main(){
 	
 	
-	
+	return 0;
 }
