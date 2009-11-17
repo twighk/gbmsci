21fa61b0156d1bdf42ee0f7f213d5598f65d5f71
tbranch88@gmail.com <tbranch88@gmail.com@581bd1f0-b7fa-11de-a433-7fce7b4d64d6>
2009-11-17 21:49:40 +0000

    skimmer makes root file and copies in tlorentz vector

--- skimmer/skimmer.C
+++ skimmer/skimmer.C
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <TFile.h>
+#include <TClonesArray.h>
+#include <TTree.h>
+#include <TLorentzVector.h>
+#include "../eventviewer/eventviewer.h"
+
+using namespace std;
 
 int main( int argc, const char* argv[] )
 {
+	TFile* fin;
+	if (argc != 2){
+		fin = new TFile("../root/AH115elec.root");
+	} else {
+		fin = new TFile(argv[1]);
+	}
+	//TFile fin("../root/AH115elec.root");
+	
+	TTree *t = dynamic_cast<TTree*>(fin->Get("bbAHCutTree"));
+	
+	TFile* fout = new TFile("fout.root", "RECREATE");
+	
+	TTree *tree = new TTree("bbAHCutTree","bbAHCutTree");
+	
+	TLorentzVector lv_electron_out;
+	
+	tree->Branch("lv_electron",&lv_electron_out);
+	
+	
+	
+	
+	eventviewer evtv(t);
+	cout <<endl<< fin->GetName()<< endl;
+	for(ULong64_t j = 0; j < evtv.totaleventnumber(); j++){
+		evtv.GetEntry(j);
+		lv_electron_out = *(dynamic_cast<TLorentzVector*> ((evtv.Getlv_electron())->At(0)));
+		tree->Fill();
+		
+		
+	}
+	tree->Print();
+	fout->Write();
+
+	
 	printf( "\nHello World\n\n" );
 	return 0;
 }
