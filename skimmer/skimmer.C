7c3053747858a7854b4041efa06b2c7615172a21
andrew.gilbert88@gmail.com <andrew.gilbert88@gmail.com@581bd1f0-b7fa-11de-a433-7fce7b4d64d6>
2009-11-18 16:58:03 +0000

    Copy electron and tau for events with 1 electron and 1 tau only

--- skimmer/skimmer.C
+++ skimmer/skimmer.C
@@ -5,6 +5,7 @@
 #include <TLorentzVector.h>
 #include <vector>
 #include <stdio.h>
+#include <TDirectory.h>
 #include "../eventviewer/eventviewer.h"
 
 using namespace std;
@@ -26,7 +27,8 @@ int main( int argc, const char* argv[] )
 	infile.push_back(new TFile("../root/Zbb.root"));
 			
 	
-	
+	Int_t number_of_electrons = 1;
+	Int_t number_of_taus = 1;
 	
 	
 	cout << "Input ROOT Files" << endl;
@@ -36,11 +38,34 @@ int main( int argc, const char* argv[] )
 		cout << "\t" << infile[i]->GetName() << endl;
 		char test[] = "";
 		strncpy(test, infile[i]->GetName(), strlen(infile[i]->GetName()) - 5);
-		test[strlen(infile[i]->GetName()) - 5] = '\0';
+		test[strlen(infile[i]->GetName()) - 5] = '\0'; //terminate string here
 		strcat(test, "_skim.root");
 		outfile.push_back(new TFile(test, "RECREATE"));
 						  
-						  
+		
+		TLorentzVector lv_electron_out;
+		TLorentzVector lv_tau_out;
+		
+		
+		TTree *intree = dynamic_cast<TTree*>(infile[i]->Get("bbAHCutTree"));
+		TTree *outtree = new TTree("bbAHCutTree","bbAHCutTree");
+		outtree->Branch("lv_electron",&lv_electron_out);
+		outtree->Branch("lv_tau",&lv_tau_out);
+
+		
+		eventviewer evtv(intree);
+		
+		for(ULong64_t j = 0; j < evtv.totaleventnumber(); j++){
+			evtv.GetEntry(j);
+			if (evtv.Getlv_electron()->GetEntriesFast() == 1 && evtv.Getlv_tau()->GetEntriesFast() == 1) {
+				lv_electron_out = *(dynamic_cast<TLorentzVector*> ((evtv.Getlv_electron())->At(0)));
+				lv_tau_out		= *(dynamic_cast<TLorentzVector*> ((evtv.Getlv_tau())->At(0)));
+				outtree->Fill();
+			
+			}
+			
+		}
+		
 		outfile[i]->Write();
 					
 	}
@@ -48,7 +73,7 @@ int main( int argc, const char* argv[] )
 	
 //	TFile fin("../root/AH115elec.root");
 //	
-//	TTree *t = dynamic_cast<TTree*>(fin->Get("bbAHCutTree"));
+//	;
 //	
 //	TFile* fout = new TFile("fout.root", "RECREATE");
 //	
