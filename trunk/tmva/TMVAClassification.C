a094d9244b706c77f19be00ad923178225882d16
tbranch88@gmail.com <tbranch88@gmail.com@581bd1f0-b7fa-11de-a433-7fce7b4d64d6>
2009-11-30 13:10:26 +0000

    hmm

--- trunk/tmva/TMVAClassification.C
+++ trunk/tmva/TMVAClassification.C
@@ -73,51 +73,51 @@ void TMVAClassification( TString myMethodList = "" )
    std::map<std::string,int> Use;
 
    Use["Cuts"]            = 1;
-   Use["CutsD"]           = 1;
-   Use["CutsPCA"]         = 1;
-   Use["CutsGA"]          = 1;
-   Use["CutsSA"]          = 1;
+   Use["CutsD"]           = 0;
+   Use["CutsPCA"]         = 0;
+   Use["CutsGA"]          = 0;
+   Use["CutsSA"]          = 0;
    // ---
-   Use["Likelihood"]      = 1;
-   Use["LikelihoodD"]     = 1; // the "D" extension indicates decorrelated input variables (see option strings)
-   Use["LikelihoodPCA"]   = 1; // the "PCA" extension indicates PCA-transformed input variables (see option strings)
-   Use["LikelihoodKDE"]   = 1;
-   Use["LikelihoodMIX"]   = 1;
+   Use["Likelihood"]      = 0;
+   Use["LikelihoodD"]     = 0; // the "D" extension indicates decorrelated input variables (see option strings)
+   Use["LikelihoodPCA"]   = 0; // the "PCA" extension indicates PCA-transformed input variables (see option strings)
+   Use["LikelihoodKDE"]   = 0;
+   Use["LikelihoodMIX"]   = 0;
    // ---
-   Use["PDERS"]           = 1;
-   Use["PDERSD"]          = 1;
-   Use["PDERSPCA"]        = 1;
-   Use["PDERSkNN"]        = 1; // depreciated until further notice
-   Use["PDEFoam"]         = 1;
+   Use["PDERS"]           = 0;
+   Use["PDERSD"]          = 0;
+   Use["PDERSPCA"]        = 0;
+   Use["PDERSkNN"]        = 0; // depreciated until further notice
+   Use["PDEFoam"]         = 0;
    // --
-   Use["KNN"]             = 1;
+   Use["KNN"]             = 0;
    // ---
-   Use["HMatrix"]         = 1;
-   Use["Fisher"]          = 1;
-   Use["FisherG"]         = 1;
-   Use["BoostedFisher"]   = 1;
-   Use["LD"]              = 1;
+   Use["HMatrix"]         = 0;
+   Use["Fisher"]          = 0;
+   Use["FisherG"]         = 0;
+   Use["BoostedFisher"]   = 0;
+   Use["LD"]              = 0;
    // ---
-   Use["FDA_GA"]          = 1;
-   Use["FDA_SA"]          = 1;
-   Use["FDA_MC"]          = 1;
-   Use["FDA_MT"]          = 1;
-   Use["FDA_GAMT"]        = 1;
-   Use["FDA_MCMT"]        = 1;
+   Use["FDA_GA"]          = 0;
+   Use["FDA_SA"]          = 0;
+   Use["FDA_MC"]          = 0;
+   Use["FDA_MT"]          = 0;
+   Use["FDA_GAMT"]        = 0;
+   Use["FDA_MCMT"]        = 0;
    // ---
-   Use["MLP"]             = 1; // this is the recommended ANN
-   Use["MLPBFGS"]         = 1; // recommended ANN with optional training method
-   Use["CFMlpANN"]        = 1; // *** missing
-   Use["TMlpANN"]         = 1; 
+   Use["MLP"]             = 0; // this is the recommended ANN
+   Use["MLPBFGS"]         = 0; // recommended ANN with optional training method
+   Use["CFMlpANN"]        = 0; // *** missing
+   Use["TMlpANN"]         = 0; 
    // ---
-   Use["SVM"]             = 1;
+   Use["SVM"]             = 0;
    // ---
-   Use["BDT"]             = 1;
+   Use["BDT"]             = 0;
    Use["BDTD"]            = 0;
-   Use["BDTG"]            = 1;
+   Use["BDTG"]            = 0;
    Use["BDTB"]            = 0;
    // ---
-   Use["RuleFit"]         = 1;
+   Use["RuleFit"]         = 0;
    // ---
    Use["Plugin"]          = 0;
    // ---------------------------------------------------------------
