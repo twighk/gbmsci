27cdb9df27ea8174bc513cfcbb46518b3282e68f
tbranch88@gmail.com <tbranch88@gmail.com@581bd1f0-b7fa-11de-a433-7fce7b4d64d6>
2010-01-11 14:59:23 +0000

    mlpsetup code furthered

---
+++ trunk/mlp/mlp.h
@@ -0,0 +1,19 @@
+#ifndef mlp_h
+#define mlp_h
+
+class mlp { 
+public:
+   mlp() {}
+   ~mlp() {}
+   double Value(int index,double in0);
+   double Value(int index, double* input);
+private:
+   double input0;
+   double neuron0x1034c87b0();
+   double neuron0x1034c8990();
+   double neuron0x1034c8bf0();
+   double neuron0x1034c8ed0();
+};
+
+#endif // mlp_h
+
