6e90f68440d1fd016981ae200b3331b4fbb4527d
tbranch88@gmail.com <tbranch88@gmail.com@581bd1f0-b7fa-11de-a433-7fce7b4d64d6>
2010-01-12 16:19:53 +0000

    histostack, legened needs fixing

--- histogram/histostack.h
+++ histogram/histostack.h
@@ -1,7 +1,13 @@
 #ifndef __histostack__
 #define __histostack__
 
+#include <vector>
+
 #include <THStack.h>
+#include <TCanvas.h>
+#include <TROOT.h>
+#include <TStyle.h>
+#include <TLegend.h>
 
 #include "Histogram.h"
 
@@ -9,16 +15,45 @@
 class Histostack {
 private:
 	static int count;//just keep track of the number of histostacks
-	THStack* hs;
+	static int canvas;//just keep track of the number of histostacks
+	THStack* histstack;
+	std::vector <Histogram *> histlist;
+	
 public:
 	Histostack(const char * name){
 		std::ostringstream str (std::ostringstream::out);
-		str << "hs" << count++ ; 
-		hs = new THStack(str.str().c_str(),name);
+		str << "histstackh" << count++ ; 
+		histstack = new THStack(str.str().c_str(),name);
+		gROOT->SetStyle("Plain");
+		gROOT->ForceStyle(); 
+		gStyle->SetOptStat(kFALSE);
+	}
+	
+	void add(Histogram & h){
+		histlist.push_back(&h);
+		histstack->Add(h.gethist());
 	}
 
+	
+	void draw(const char * legendname =""){
+		std::ostringstream str (std::ostringstream::out);
+		str << "histstackc" << canvas++ ; 
+		TCanvas* my_canvas = new TCanvas(str.str().c_str(),"test" ,200,10,600,400); 
+		
+		TLegend *legend = new TLegend(0.1,0.7,0.48,0.9,legendname);
+		legend->SetTextSize(0.03);
+		for (unsigned int i = 0; i != histlist.size(); ++i) {
+			legend -> AddEntry(histlist[i]->gethist(),"","f");
+		}
+		
+		histstack->Draw();
+		legend->Draw(); // must be after other draws to draw on top
+
+	
+	}
 };
 int Histostack::count = 0;
+int Histostack::canvas = 0;
 
 
 
