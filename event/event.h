1030b3810ed08a501d5d2489b9090d48b095ed70
andrew.gilbert88@gmail.com <andrew.gilbert88@gmail.com@581bd1f0-b7fa-11de-a433-7fce7b4d64d6>
2010-01-25 13:51:46 +0000

    Event class

---
+++ event/event.h
@@ -0,0 +1,24 @@
+#ifndef __event__
+#define __event__
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <TFile.h>
+#include <TTree.h>
+#include <TClonesArray.h>
+#include <TLorentzVector.h>
+#include <cmath>
+#include <sstream>
+#include <TString.h>
+
+
+class event{
+
+	private
+
+
+};
+
+
+#endif
