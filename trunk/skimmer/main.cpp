e5b892adf0712568a30e08e8914118862ae3980c
tbranch88@gmail.com <tbranch88@gmail.com@581bd1f0-b7fa-11de-a433-7fce7b4d64d6>
2010-01-28 11:29:59 +0000

    mlp changes

--- trunk/skimmer/main.cpp
+++ trunk/skimmer/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ #include <iostream>
 #include <vector>
 #include <string>
 #include "skimmer.h"
@@ -6,28 +6,49 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    
-    skimmer a("../root/");
-    //	a.DoTauCuts();
-    a.AddChannel("AH115","bbAHCutTree", 7852.0);
-    a.AddChannel("AH115bb","bbAHCutTree", 3831.0);
-    a.AddChannel("AH160","bbAHCutTree", 36823.0);
-    a.AddChannel("AH160bb","bbAHCutTree", 11400.0);
-    //	a.AddChannel("QCD_BCtoE_Pt20to30","bbAHCutTree", 1);
-    //	a.AddChannel("QCD_BCtoE_Pt30to80","bbAHCutTree", 1);
-    //	a.AddChannel("QCD_BCtoE_Pt80to170","bbAHCutTree", 1);
-    //	a.AddChannel("QCD_EMenriched_Pt20to30","bbAHCutTree", 1);
-    //	a.AddChannel("QCD_EMenriched_Pt30to80","bbAHCutTree", 1);
-    //	a.AddChannel("QCD_EMenriched_Pt80to170","bbAHCutTree", 1);
-    a.AddChannel("TTplusJets","bbAHCutTree", 2562.0);
-    a.AddChannel("WplusJets","bbAHCutTree", 243.0);
-    a.AddChannel("Zbb","bbAHCutTree", 3483.0);
-    a.AddChannel("Zcc","bbAHCutTree", 3483.0);
-    a.AddChannel("ZplusJets","bbAHCutTree", 331.0);
-    
-    a.GoSkim();
-    a.WriteCombo();
-    
-    
-    
+
+	skimmer a;
+	a.SetRootPath("../root/");
+	a.DoTauCuts();
+	a.AddChannel("AH115","bbAHCutTree", 7852.0);
+//	a.AddChannel("AH115bb","bbAHCutTree", 3831.0);
+//	a.AddChannel("AH160","bbAHCutTree", 36823.0);
+//	a.AddChannel("AH160bb","bbAHCutTree", 11400.0);
+////	a.AddChannel("QCD_BCtoE_Pt20to30","bbAHCutTree", 1);
+////	a.AddChannel("QCD_BCtoE_Pt30to80","bbAHCutTree", 1);
+////	a.AddChannel("QCD_BCtoE_Pt80to170","bbAHCutTree", 1);
+////	a.AddChannel("QCD_EMenriched_Pt20to30","bbAHCutTree", 1);
+////	a.AddChannel("QCD_EMenriched_Pt30to80","bbAHCutTree", 1);
+////	a.AddChannel("QCD_EMenriched_Pt80to170","bbAHCutTree", 1);
+//	a.AddChannel("TTplusJets","bbAHCutTree", 2562.0);
+//	a.AddChannel("WplusJets","bbAHCutTree", 243.0);
+//	a.AddChannel("Zbb","bbAHCutTree", 3483.0);
+//	a.AddChannel("Zcc","bbAHCutTree", 3483.0);
+	a.AddChannel("ZplusJets","bbAHCutTree", 331.0);
+
+	a.AddVarElectronEt();
+	a.AddVarTauEt();
+	a.AddVarMetEt();
+	a.AddVarElectronTauDPhi();
+	a.AddVarElectronEta();
+	a.AddVarTauEta();
+	a.AddVarElectronEcalIso();
+	a.AddVarElectronHcalIso();
+	a.AddVarElectronTrackIso();
+//	a.AddVarElectronCharge();
+//	a.AddVarTauCharge();
+	a.AddVarElecTauChargeProd();
+//	a.AddVarTauProng();
+//	a.AddVarTauLeadTrk();
+//	a.AddVarTauEcalIso();
+//	a.AddVarTauTrackIso();
+//	a.AddVarTauAntiElectron();
+	a.AddVarElectronMetDPhi();
+	a.AddVarElectronMetMt();
+	a.AddVarVisibleMass();
+	a.GoSkim();
+	a.WriteCombo();
+
+	
+	
 }
