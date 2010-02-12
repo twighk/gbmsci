b0afd3c6552de7d8d715431cd21650830014652d
andrew.gilbert88@gmail.com <andrew.gilbert88@gmail.com@581bd1f0-b7fa-11de-a433-7fce7b4d64d6>
2010-02-12 10:25:11 +0000

    try setting default btag to zero instead of -13

--- trunk/header/varlib.h
+++ trunk/header/varlib.h
@@ -255,7 +255,7 @@ public:
 	virtual Double_t Get(std::map<std::string, brptr> * data, std::map<std::string, Int_t> * indexinfo){
         std::vector<Double_t> * btag = u< std::vector<Double_t> >((*data)["jetBTagTrackCountHighEff"]);
         if ( (*indexinfo)["bindex"] == -1  ) {
-            return -13.; //Default lowest tab value
+            return 0.; //Default lowest tab value
         } else {
             return (*btag)[((*indexinfo)["bindex"])];
         }
