3a44043e3f66d9fc05bdb3f2cafac0f06d8078d7
tbranch88@gmail.com <tbranch88@gmail.com@581bd1f0-b7fa-11de-a433-7fce7b4d64d6>
2010-02-12 15:26:01 +0000

    started moveing mlp to class format

--- mlp/mplsetup.cpp
+++ mlp/mplsetup.cpp
@@ -14,90 +14,8 @@
 #include <string>
 #include <sstream>
 
+#include "mlpsetup.h"
 using namespace std;
-
-struct ChannelMeta {
-	Int_t begin;
-	Int_t end;
-};
-
-void mlpsetup(TTree *tree,string file,  Int_t ntrain=101){
-	cout << "ntrain: " << ntrain << endl;
-	if (!gROOT->GetClass("TMultiLayerPerceptron")) {
-		gSystem->Load("libMLP");
-	}
-
-	vector <string> branchnames; //List of branch names (but not types)
-	vector <string> types; // List of type names (type1, type2, etc.)
-	
-	TObjArray* lob = tree->GetListOfBranches();
-	for (Int_t i = 0; i < lob->GetEntriesFast(); ++i) {
-		TBranch * branch = (TBranch *)lob->At(i);
-		string bname = string(branch -> GetName());
-		if (string(bname) == "weight"
-		  ||string(bname) == "IntLum"
-			) {
-			continue;
-		} else if(string(bname).compare(0,4,"type")){
-			branchnames.push_back(bname);
-		} else {
-			types.push_back(bname);
-		}
-	}// get list of all branches, and separate out types
-
-	
-//generate mlp stting in outstrem
-	ostringstream outstrm(ostringstream::out);
-	for (unsigned int i = 0; i < branchnames.size(); ++i) {
-		outstrm << "@" << branchnames[i];
-		if (i != branchnames.size()-1) {
-			outstrm << ",";
-		}
-	} // list branch names
-	
-	outstrm << ":"  << branchnames.size() << ":";// Neural net structure
-		
-	
-	for (unsigned int i = 0; i < types.size(); ++i) {
-		outstrm << types[i];
-		if (i != types.size()-1) {
-			outstrm << ",";
-		}
-	}// output vars
-	
-	cout << outstrm.str() << endl; // print it out, to see what went in
-
-	TMultiLayerPerceptron *mlp = 
-	new TMultiLayerPerceptron(outstrm.str().c_str(),
-							  "weight",
-							  tree,
-							  "Entry$%2==0",
-							  "(Entry$+1)%2==0"); // set up neural net
-	
-	mlp->SetLearningMethod(TMultiLayerPerceptron::kBFGS); // choose learning method
-	
-	mlp->Train(ntrain, "text,graph,update=10"); // train
-	mlp->Export(string("mlp" +file).c_str(),"C++"); // save data
-	
-	
-
-	TMLPAnalyzer ana(mlp);
-	// Initialisation
-	ana.GatherInformations();
-	// output to the console
-	ana.CheckNetwork();
-	TCanvas* mlpa_canvas1 = new TCanvas("mlpa_canvas","Network analysis");
-	// shows how each variable influences the network
-	ana.DrawDInputs();
-	TCanvas* mlpa_canvas2 = new TCanvas("mlpa_canvas2","Network analysis");
-	// shows the network structure
-	mlp->Draw();
-	TCanvas* mlpa_canvas3 = new TCanvas("mlpa_canvas3","Network analysis");
-	// draws the resulting network
-	ana.DrawNetwork(0,"type==1","type==0");
-	
-	
-}
 
 int main(int argc, char** argv){
 // Setup X for graph output
@@ -116,7 +34,10 @@ int main(int argc, char** argv){
 
 	
 //run mlp
-	mlpsetup(t, a); 
+	MlpSetup mlp(t, a); 
+	
+	
+	mlp.runsetup();
 	
 //wait, so graphs are shown
 	cerr << "Hanging for X11" << endl;
