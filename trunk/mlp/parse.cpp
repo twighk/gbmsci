67aff33bfd39fe9d62fc5647de03a7ed37e07cc0
andrew.gilbert88 <andrew.gilbert88@581bd1f0-b7fa-11de-a433-7fce7b4d64d6>
2010-02-24 12:59:54 +0000

    finished twohist class (for now)

--- trunk/mlp/parse.cpp
+++ trunk/mlp/parse.cpp
@@ -2,15 +2,25 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <TApplication.h>
+#include <TROOT.h>
+
+#include "../header/twohist.h"
 
 using namespace std;
 
-int main(){
-	ifstream fin("out.csv");
+int main(int argc, char** argv){
+    TApplication theApp("App", &argc, argv); // this must be instantiated only once 
+
+    
+    
+    ifstream fin("out.csv");
 	
 	char chars[500];
 	
 	double a, b, c, d;
+    TH1F* train = new TH1F("train","train",100,0,1);
+    TH1F* test = new TH1F("test","test",100,0,1);
 	
 	while (!fin.eof()){
 		fin.getline (chars,500);
@@ -29,10 +39,18 @@ int main(){
 				stringstream ss (stringstream::in | stringstream::out);
 				ss << str;
 				ss >> a >> b >> c >> d;
-				cout << "a: " << a << " b: " << b << " c: " << c << " d: " << d << endl;
+                train->Fill(c / a);
+                test->Fill(d / b);
+//				cout << "a: " << a << " b: " << b << " c: " << c << " d: " << d << endl;
 				
 			}
 		}
 	}
+    
+    TwoHist hists(train, test, "Error @ 200 epochs for BFGS with 9 nodes","Fractional Error","Entries");
+    hists.Plot();
+    
+    cerr << "Hanging for X11" << endl;
+    theApp.Run();
 	return 0;
 }
