c85257563c26f21ab411192c9eaa8742d684b262
tbranch88@gmail.com <tbranch88@gmail.com@581bd1f0-b7fa-11de-a433-7fce7b4d64d6>
2010-03-17 13:34:46 +0000

    separation plots

--- separation/main.cpp
+++ separation/main.cpp
@@ -21,12 +21,33 @@
 
 using namespace std;
 
+struct xyz {
+	int x;
+	int y;
+	int z;
+
+	xyz(int x, int y, int z):x(x),y(y),z(z){ };
+};
+
 int main(int argc, char ** argv){
 	TApplication theApp("App", &argc, argv); // this must be instantiated only once 
 
 	
 	vector < string > names;
 	vector < string > branches;
+//	names.push_back("AH115");
+//	names.push_back("AH115bb");
+//	names.push_back("AH160");
+//	names.push_back("AH160bb");
+//	names.push_back("QCD_BCtoE");
+//	names.push_back("QCD_EMenriched");
+//	names.push_back("TTplusJets");
+//	names.push_back("WplusJets");
+//	names.push_back("Zbb");
+//	names.push_back("Zcc");
+//	names.push_back("ZplusJets");
+	
+	
 	names.push_back("AH115");
 	names.push_back("AH115bb");
 	names.push_back("AH160");
@@ -140,65 +161,117 @@ int main(int argc, char ** argv){
 		++z;
 	}
 /*****************************/
-	const int num = 7;
-	const int hist1num = 8;
-	const int hist2num = 2;
+//	const int num = 7;
+//	const int hist1num = 8;
+//	const int hist2num = 2;
 /*****************************/
     
-    if (num >= branches.size()){
-        cerr << "Histogram number must be less than " << branches.size() << endl;
-        exit(-1);
-    }
-	
-	gROOT->SetStyle("Plain");
-	
-	TH1F *hist1 = new TH1F();
-	TH1F *hist2 = new TH1F();
-	
-	*hist1 = *(branchhists[num][hist1num]);
-	*hist2 = *(branchhists[num][hist2num]);
-	
-	ostringstream histtitle;
-	histtitle << branches[num] << ": " << names[hist1num]<< " vs. "<< names[hist2num];
-	cout << histtitle.str() <<endl;
-	hist1->SetTitle(histtitle.str().c_str());
-	hist2->SetTitle(histtitle.str().c_str());
-	hist1->SetName(names[hist1num].c_str());
-	hist2->SetName(names[hist2num].c_str());
-	
-	TCanvas *canvas = new TCanvas();
-	hist1->SetFillColor(6);
-	hist1->SetFillStyle(3002);
-	hist2->SetFillColor(4);
-	hist2->SetFillStyle(3003);
-		
-	TLegend *legend = new TLegend(0.78,0.45,0.98,0.6,"");
-	legend -> SetTextSize(0.03);
-	legend -> AddEntry(hist1,names[hist1num].c_str(),"f");
-	legend -> AddEntry(hist2,names[hist2num].c_str(),"f");
-	
-	Float_t count1, count2;
-	count1 = hist1->GetBinContent(hist1->GetMaximumBin());
-	count2 = hist2->GetBinContent(hist2->GetMaximumBin());
-	if (count1 >= count2){
-		hist1->Draw();
-		hist2->Draw("sames");
-	} else {
-		hist2->Draw();
-		hist1->Draw("sames");
-	}
-	legend->Draw();
-	
-	canvas->Update();
-	
-	
-	TPaveStats * st = (TPaveStats *) hist2 ->FindObject("stats");
-	st->SetY2NDC(0.80);
-	st->SetY1NDC(0.65);
-	
 
-	canvas->Modified();
+	vector <xyz> list;
+//	list.push_back(xyz(0,7,10));
+//	list.push_back(xyz(0,7,5));
+//	list.push_back(xyz(0,1,0));
+//	list.push_back(xyz(1,5,0));
+//	list.push_back(xyz(1,5,6));
+//	list.push_back(xyz(2,5,0));
+//	list.push_back(xyz(2,5,6));
+//	list.push_back(xyz(3,5,10));
+//	list.push_back(xyz(3,7,6));
+//	list.push_back(xyz(3,7,0));
+//	list.push_back(xyz(3,7,2));
+//	list.push_back(xyz(5,4,0));
+//	list.push_back(xyz(7,7,3));
+//	list.push_back(xyz(7,4,3));
+//	list.push_back(xyz(7,4,10));
+//	list.push_back(xyz(8,3,7));
+//	list.push_back(xyz(8,2,7));
+//	list.push_back(xyz(8,1,7));
+//	list.push_back(xyz(8,10,7));
+//	list.push_back(xyz(11,5,10));
+//	list.push_back(xyz(11,5,3));
+//	list.push_back(xyz(11,3,6));
+//	list.push_back(xyz(11,3,5));
+//	list.push_back(xyz(26,5,10));
+//	list.push_back(xyz(26,5,3));
+//	list.push_back(xyz(26,3,6));
+//	list.push_back(xyz(26,3,5));
+//	list.push_back(xyz(13,10,7));
+//	list.push_back(xyz(15,4,7));
+//	list.push_back(xyz(17,6,7));
+//	list.push_back(xyz(17,1,7));
+//	list.push_back(xyz(17,6,4));
+//	list.push_back(xyz(20,2,5));
+//	list.push_back(xyz(20,3,6));
+//	list.push_back(xyz(23,10,2));
+//	list.push_back(xyz(23,10,5));
+//	list.push_back(xyz(23,1,0));
+//	list.push_back(xyz(23,8,2));
+//	list.push_back(xyz(23,8,5));
+	list.push_back(xyz(24,10,7));
 	
+	
+	for (unsigned int i = 0; i != list.size(); ++i) {
+		int num = list[i].x;
+		int hist1num = list[i].y;
+		int hist2num = list[i].z;
+	
+		
+		
+		if (num >= branches.size()){
+			cerr << "Histogram number must be less than " << branches.size() << endl;
+			exit(-1);
+		}
+		
+		gROOT->SetStyle("Plain");
+		
+		TH1F *hist1 = new TH1F();
+		TH1F *hist2 = new TH1F();
+		
+		*hist1 = *(branchhists[num][hist1num]);
+		*hist2 = *(branchhists[num][hist2num]);
+		
+		ostringstream histtitle;
+		histtitle << branches[num] << ": " << names[hist1num]<< " vs. "<< names[hist2num];
+		cout << histtitle.str() << endl;
+		hist1->SetTitle(histtitle.str().c_str());
+		hist2->SetTitle(histtitle.str().c_str());
+		hist1->SetName(names[hist1num].c_str());
+		hist2->SetName(names[hist2num].c_str());
+		
+		TCanvas *canvas = new TCanvas();
+		hist1->SetFillColor(6);
+		hist1->SetFillStyle(3002);
+		hist2->SetFillColor(4);
+		hist2->SetFillStyle(3003);
+			
+		TLegend *legend = new TLegend(0.78,0.45,0.98,0.6,"");
+		legend -> SetTextSize(0.03);
+		legend -> AddEntry(hist1,names[hist1num].c_str(),"f");
+		legend -> AddEntry(hist2,names[hist2num].c_str(),"f");
+		
+		Float_t count1, count2;
+		count1 = hist1->GetBinContent(hist1->GetMaximumBin());
+		count2 = hist2->GetBinContent(hist2->GetMaximumBin());
+		if (count1 >= count2){
+			hist1->Draw();
+			hist2->Draw("sames");
+		} else {
+			hist2->Draw();
+			hist1->Draw("sames");
+		}
+		legend->Draw();
+		
+		canvas->Update();
+		
+		
+		TPaveStats * st = (TPaveStats *) hist2 ->FindObject("stats");
+		st->SetY2NDC(0.80);
+		st->SetY1NDC(0.65);
+		
+
+		canvas->Modified();
+	}
+
 	
 	
 	cerr << "Hanging for X11" << endl;
